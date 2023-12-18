24c67270197f2e0e00cc1b0bf8cce155494caba0
mananispiwpiw <rafijaeger@gmail.com>
2023-12-18 23:25:42 +0700

    simple web server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	address := "localhost:8080"
+	fmt.Printf("Server started at %s\n", address)
+	err := http.ListenAndServe(address, mux)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
